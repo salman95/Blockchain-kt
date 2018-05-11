eab3823b2b096b827a2847932f41838d0f3ce4fd
Salman Bobat <salman.9515@outlook.com>
2018-05-11 16:04:08 -0500

    Changed sha256hex to sha512hex
    Changed hashing algorithms

---
+++ Block.kt
@@ -0,0 +1,15 @@
+import org.apache.commons.codec.digest.DigestUtils
+import java.util.*
+
+class Block (val index: Int, val previousHash: String, val data: Any) {
+
+    val timestamp: Long = Date().time
+    val hash = calculateHash() // hash = returned value of calculate hash
+
+    fun calculateHash(): String { //returns string
+        val input = (index.toString() + previousHash + timestamp + data).toByteArray()
+        return DigestUtils.sha512Hex(input) // returns the input in sha512Hex conversion
+
+    }
+}
+
